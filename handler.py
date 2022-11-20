bc6869e50c091bdd821a826c39a73efbecb84f5b
camden <camden@debian.main>
2022-11-20 15:33:18 -0500

    addition of new functions for web scrape handler

--- handler.py
+++ handler.py
@@ -1,10 +1,12 @@
-import sys
-import requests
 import logging
+import sys
+
+import requests
 
 logging.basicConfig(filename='web_scrape.log', level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
 
-# todo: if sold out at tustin store not found, search for in stock string identifier
+# done: if sold out at tustin store not found, search for in stock string identifier
+
 # todo: setup mail account to send text in case of in stock confirmation
     # todo: in text, include store phone number
 # todo: schedule job or loop
@@ -26,14 +28,18 @@ def get_target_urls():
             urls.append(line.strip())
     return urls
 
-def check_html_for_sold_out():
+def check_html_for_sold_out(url):
     with open('web_to_text.txt', 'r') as file:
         file_as_text = file.read()
-        target_string_placement = file_as_text.find('SOLD OUT<span class="storeName"> at Tustin Store</span>')
-        if target_string_placement == -1:
-            logging.info('SOLD OUT string not found - assume item in stock -  target_string_placement = {}'.format(target_string_placement))
+        target_sold_out_string_placement = file_as_text.find('SOLD OUT<span class="storeName"> at Tustin Store</span>')
+        if target_sold_out_string_placement == -1:
+            target_in_stock_string_placement = file_as_text.find('IN STOCK</span></span><span class="storeName"> at Tustin Store</span></span>')
+            if target_in_stock_string_placement == -1:
+                logging.info('FAILURE - NEITHER FOUND - product url = {} - target_in_stock_string_placement = {}'.format(url, target_in_stock_string_placement))            
+            else:
+                logging.info('SUCCESS - IN STOCK string found - product url = {} - target_in_stock_string_placement = {}'.format(url, target_in_stock_string_placement))
         else:
-            logging.info('SOLD OUT string found - out of stock - target_string_placement = {}'.format(target_string_placement))    
+            logging.info('FAILURE - SOLD OUT string found - out of stock - product url = {} - target_sold_out_string_placement = {}'.format(url, target_sold_out_string_placement))    
     return None
 
 def is_sold_out():
@@ -41,7 +47,7 @@ def is_sold_out():
     for url in get_target_urls():
         result = requests.get(url=url, headers=headers)
         save_webpage(result.content)
-        check_html_for_sold_out()
+        check_html_for_sold_out(url)
     return None
 
 if __name__ == '__main__':
